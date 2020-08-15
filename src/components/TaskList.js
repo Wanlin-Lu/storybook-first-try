ed93455919e33864bba3a2c1f9a7c39cb755c619
Wanlin-Lu <luwanlin@126.com>
2020-08-15 09:15:01 +0800

    connect TaskList to redux store

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,8 +4,10 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import Task from "./Task";
+import { connect } from "react-redux";
+import { archiveTask, pinTask } from "../lib/redux";
 
-function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
     onArchiveTask,
@@ -70,4 +72,14 @@ TaskList.defaultProps = {
   loading: false,
 };
 
-export default TaskList;
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(
+      (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+    ),
+  }),
+  (dispatch) => ({
+    onArchiveTask: (id) => dispatch(archiveTask(id)),
+    onPinTask: (id) => dispatch(pinTask(id)),
+  })
+)(PureTaskList);
